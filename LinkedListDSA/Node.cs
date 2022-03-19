cf06a60e425c08dd74b1206c9e17a154e564b18b
Kanhaiya Tyagi <kanhaiyatyagi63@gmail.com>
2022-03-19 19:41:22 +0530

    LL - Insert Single node

---
+++ LinkedListDSA/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDSA
+{
+    public class Node
+    {
+        public int data { get; set; }
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+
+    }
+}
--- LinkedListDSA/Program.cs
+++ LinkedListDSA/Program.cs
@@ -2,18 +2,7 @@
 
 namespace LinkedListDSA
 {
-    public class Node
-    {
-        public int data { get; set; }
-        public Node next;
 
-        public Node(int data)
-        {
-            this.data = data;
-            next = null;
-        }
-
-    }
     class Program
     {
         void PrintLinkedList(Node head)
@@ -26,31 +15,57 @@ namespace LinkedListDSA
                 temp = temp.next;
                 counter++;
             }
-            while (temp != null)
-            {
-                Console.WriteLine($"Node {counter} => {temp.data}");
-                temp = temp.next;
-                counter++;
-            }
-
         }
+
+        Node InsertNode(Node head, int data, int index)
+        {
+            // handling less than 0 index
+            if (index < 0)
+                return head;
+
+            Node temp = head; // temp node for maintaining head
+            Node newNode = new Node(70);
+            var count = 0;
+            // handling insert at 0;
+            if (index == 0)
+            {
+                newNode.next = head;
+                head = newNode;
+                return head;
+            }
+            // handling index out of bound and adding condition
+            while (count < index - 1 && temp != null)
+            {
+                temp = temp.next;
+                count++;
+            }
+            if (temp != null)
+            {
+                newNode.next = temp.next;
+                temp.next = newNode;
+            }
+            return head; // return current head
+        }
+
 
         static void Main(string[] args)
         {
 
             Console.WriteLine("Hello Linked List!");
             Node head;
-            var n1 = new Node(10);
+            var n1 = new Node(1);
             head = n1;
-            var n2 = new Node(200);
+            var n2 = new Node(2);
             n1.next = n2;
-            var n3 = new Node(30);
+            var n3 = new Node(3);
             n2.next = n3;
-            var n4 = new Node(40);
+            var n4 = new Node(4);
             n3.next = n4;
             var n5 = new Node(5);
             n4.next = n5;
-            new Program().PrintLinkedList(head);
+
+            var header = new Program().InsertNode(head, 70, -1);
+            new Program().PrintLinkedList(header);
 
         }
     }
